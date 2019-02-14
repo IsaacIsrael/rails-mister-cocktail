41bf63a60e26881339e681333dc6f27e148131d0
isaac israel <email.iisrael@gmail.com>
2019-02-14 14:44:14 -0200

    Initialize Dose  create

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -2,11 +2,23 @@
 
 # Doses Controller
 class DosesController < ApplicationController
-  before_action :set_cocktail, only: %i[new]
+  before_action :set_cocktail, only: %i[new create]
+  before_action :set_ingredient, only: %i[create]
 
   def new
     @dose = Dose.new
     @ingredients = Ingredient.all
+  end
+
+  def create
+    @dose = @cocktail.doses.build(doses_params)
+    @dose.ingredient = @ingredient
+
+    if @dose.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
   end
 
   private
@@ -15,7 +27,11 @@ class DosesController < ApplicationController
     @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
+  def set_ingredient
+    @ingredient = Ingredient.find(params[:dose][:ingredient])
+  end
+
   def doses_params
-    params.require(:cocktail).permit(:name, :ingredient)
+    params.require(:dose).permit(:description)
   end
 end
