1d940e47ca81e90793c1d9584f206b47c11977d5
isaac israel <email.iisrael@gmail.com>
2019-02-14 13:54:37 -0200

    Initialize Cocktail show

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,7 +2,17 @@
 
 # Cocktails Controller
 class CocktailsController < ApplicationController
+  before_action :set_coktail, only: %i[show]
+
   def index
     @cocktails = Cocktail.all
+  end
+
+  def show; end
+
+  private
+
+  def set_coktail
+    @cocktail = Cocktail.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@
 Rails.application.routes.draw do
   get 'cocktails/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: %i[index]
+  resources :cocktails, only: %i[index show]
 end
