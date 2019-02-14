2fa44246a2fe313c979b667ac563ea73606b6be5
isaac israel <email.iisrael@gmail.com>
2019-02-14 14:01:00 -0200

    Initialize Cocktail new

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,13 +2,17 @@
 
 # Cocktails Controller
 class CocktailsController < ApplicationController
-  before_action :set_coktail, only: %i[show]
+  before_action :set_coktail, only: %i[showroute]
 
   def index
     @cocktails = Cocktail.all
   end
 
   def show; end
+
+  def new
+    @cocktail = Cocktail.new
+  end
 
   private
 
