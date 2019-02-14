c6b2f4067b1f97b7eca5f92552b1d0d9d161a0f8
isaac israel <email.iisrael@gmail.com>
2019-02-14 15:12:52 -0200

    Remove the genration routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'cocktails/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: %i[index show new create] do
     resources :doses, only: %i[new create]
   end
+
+  resources :doses, only: %i[destroy]
 end
