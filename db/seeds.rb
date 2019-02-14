d3d60a6e1902a2501dc45746bce400b40cd2342d
isaac israel <email.iisrael@gmail.com>
2019-02-14 13:38:44 -0200

    Update Seed to get Ingredient form thecocktaildb API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 # This file should contain all the record creation needed to seed the database
 # with its default values.
 # The data can then be loaded with the rails db:seed command (or created
@@ -14,7 +16,10 @@ puts 'Clean data Base'
 Ingredient.destroy_all
 
 puts 'Start creating the Ingredient'
-Ingredient.create(name: 'lemon')
-Ingredient.create(name: 'ice')
-Ingredient.create(name: 'mint leaves')
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+response = JSON.parse(URI.parse(url).open.read)
+
+response['drinks'].each { |i| Ingredient.create(name: i['strIngredient1']) }
+
 puts 'Finish creating the Ingredient'
